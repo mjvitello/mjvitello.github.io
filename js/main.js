557fc86e6ec83f6c12a693a700c65e6aaed7c534
mattvitello <vitellofellow@gmail.com>
2018-09-30 15:00:32 -0400

    angular site now

--- js/main.js
+++
@@ -1,8 +0,0 @@
-console.log(`%c 
-  ___  _____   __  ___ _      _   ___ ___ 
- | _ \\/ _ \\ \\ / / | _ ) |    /_\\ |_ _| _ \\
- |   / (_) \\ V /  | _ \\ |__ / _ \\ | ||   /
- |_|_\\\\___/ |_|   |___/____/_/ \\_\\___|_|_\\
-                                          
-`, "font-family:monospace");
-console.log("site by matt vitello https://mattvitello.com/")
